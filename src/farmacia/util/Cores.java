29bb48265769712576b7aa4ec8e844ef1c8a1e29
Renata Negrini <renatanegrinim@gmail.com>
2025-05-01 19:26:41 -0300

    feat: adiciona classe Cores em util para aplicar cor verde no console

---
+++ src/farmacia/util/Cores.java
@@ -0,0 +1,8 @@
+package farmacia.util;
+
+public class Cores {
+	// Códigos ANSI para cores
+    public static final String VERDE = "\u001B[32m";
+    public static final String RESET = "\u001B[0m";
+    
+}
