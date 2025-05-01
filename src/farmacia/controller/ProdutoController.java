b477c57ce6866fba9da4db68936d4d8a2f65f28e
Renata Negrini <renatanegrinim@gmail.com>
2025-05-01 17:42:31 -0300

    feat: cria ProdutoController com implementação da interface ProdutoRepository

---
+++ src/farmacia/controller/ProdutoController.java
@@ -0,0 +1,38 @@
+package farmacia.controller;
+
+import farmacia.repository.ProdutoRepository;
+
+public class ProdutoController implements ProdutoRepository {
+
+	@Override
+	public void criarProduto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarTodosProdutos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void consultarProdutoPorId() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizarProduto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletarProduto() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
