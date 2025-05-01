6bb0962380e355bc010fe15c6d44b4fffd584c9a
Renata Negrini <renatanegrinim@gmail.com>
2025-05-01 18:03:40 -0300

    feat: implementa método listarTodosProdutos da interface ProdutoRepository em ProdutoController

--- src/farmacia/controller/ProdutoController.java
+++ src/farmacia/controller/ProdutoController.java
@@ -1,10 +1,15 @@
 package farmacia.controller;
 
+import java.util.ArrayList;
+
 import farmacia.model.Produto;
 import farmacia.repository.ProdutoRepository;
 
 public class ProdutoController implements ProdutoRepository {
 
+	private ArrayList<Produto> produtosCadastrados = new ArrayList<Produto>();
+	int id = 0;
+	
 	@Override
 	public void criarProduto(Produto produto) {
 		// TODO Auto-generated method stub
@@ -13,7 +18,9 @@ public class ProdutoController implements ProdutoRepository {
 
 	@Override
 	public void listarTodosProdutos() {
-		// TODO Auto-generated method stub
+		for(var produto : produtosCadastrados) {
+			produto.visualizar();
+		}
 		
 	}
 
